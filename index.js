a97a25b9a8f66b168bca90f86efc369fc5903ccb
RuslanIbragimov5 <galaxy242@mail.ru>
2022-01-25 20:45:31 +0300

    .

---
+++ index.js
@@ -0,0 +1,20 @@
+// 'use strict';
+
+let isOnline = true;
+
+const age = 10;
+function someName() {
+  {
+    {
+      alert(age);
+    }
+  }
+}
+
+const userName = prompt("Ввведите имя");
+const userEmail = prompt("Ввведите почту");
+const userPassword = prompt("Ввведите пароль");
+
+alert(`
+    name: ${userName}, email: ${userEmail}, password: ${userPassword}
+`);
